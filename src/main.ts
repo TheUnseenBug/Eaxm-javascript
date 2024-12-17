8351896b41e41c171b024b081b1478128ba61e07
dennis granheimer <dennis.granheimer@live.se>
2024-12-17 13:31:39 +0100

    beginning of project api and display function started

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,25 @@
-import "./style.css";
-https://www.omdbapi.com/?apikey=d83aa95&
+import axios from "axios"
+import Movie from "./types"
+
+async function serverRequest()  {
+const popular = await axios({
+    method: 'get',
+    url: 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMjVmMjdlN2NmYzQ4NGJhZTQyM2UxNDQyYjkxNmUxNiIsIm5iZiI6MTcwMDY5NDA1MC44NjA5OTk4LCJzdWIiOiI2NTVlODgyMjdkZmRhNjAxMWJhZTUxNmQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.GWVFB8R4UZZayQOVqkvIeLtUbhdkUBQrBJnMOkYYYwQ'
+      }
+}).then((data) => console.log(data.data.results))
+console.log(popular)
+
+const byId = {}
+
+const getPoster = {}
+}
+serverRequest()
+
+function displayMovies(movies: Movie[]) {
+movies.forEach((movie) => {
+    
+})
+}
